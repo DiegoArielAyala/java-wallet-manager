83784bf2a45e13d0533496c2c3add58ae62e267a
DiegoArielAyala <diegoa.ayala@gmail.com>
2025-01-25 22:43:19 +0100

    Creamos Wallet.java

--- chaincode/java-wallet-manager/src/main/java/com/blockchainempresarial/hyperledger/fabric/samples/wallet/Wallet.java
+++ chaincode/java-wallet-manager/src/main/java/com/blockchainempresarial/hyperledger/fabric/samples/wallet/Wallet.java
@@ -1,10 +1,4 @@
-/*
-	Business Blockchain Training & Consulting SpA. All Rights Reserved.
-	www.blockchainempresarial.com
-	email: ricardo@blockchainempresarial.com
-*/
-
-package com.blockchainempresarial.hyperledger.fabric.samples.wallet;
+package main.java.com.blockchainempresarial.hyperledger.fabric.samples.wallet;
 
 import java.util.Objects;
 
@@ -16,49 +10,59 @@ import com.owlike.genson.annotation.JsonProperty;
 @DataType()
 public final class Wallet {
 
-	@Property()
-	private final Double tokenAmount;
+    @Property()
+    private final Double tokenAmount;
 
-	@Property()
-	private final String owner;
+    @Property()
+    private final String owner;
 
-	public Double getTokenAmount() {
-		return tokenAmount;
-	}
+    public Double getTokenAmount() {
+        return tokenAmount;
+    }
 
-	public String getOwner() {
-		return owner;
-	}
+    public String getOwner() {
+        return owner;
+    }
 
-	public Wallet(@JsonProperty("tokenAmount") final Double tokenAmount, @JsonProperty("owner") final String owner) {
-		this.tokenAmount = tokenAmount;
-		this.owner = owner;
-	}
+    public Wallet(@JsonProperty("tokenAmount") final Double tokenAmount, @JsonProperty("owner") final String owner) {
+        // Usamos el @Json para definir el nombre de la propiedad
+        this.tokenAmount = tokenAmount;
+        this.owner = owner;
+    }
 
-	@Override
-	public boolean equals(final Object obj) {
-		if (this == obj) {
-			return true;
-		}
+    // Es importante sobreescribir el metodo equals para cuando comparemos wallet
+    // contra wallet saber lo que vas a comparar.
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if ((obj == null) || (getClass() != obj.getClass())) {
+            return false;
+        }
+        // Comprobamos que el objeto sea igual a este, que no sea nulo y que la clase
+        // coincida
 
-		if ((obj == null) || (getClass() != obj.getClass())) {
-			return false;
-		}
+        Wallet other = (Wallet) obj; // Creamos un objeto tipo wallet
 
-		Wallet other = (Wallet) obj;
+        // Hacemos una comparacion uno a uno con deepEquals:
+        return Objects.deepEquals(new String[] { getTokenAmount().toString().getOwner() },
+                new String[] { other.getTokenAmount(), other.getOwner() });
+    }
 
-		return Objects.deepEquals(new String[] { getTokenAmount().toString(), getOwner() },
-				new String[] { other.getTokenAmount().toString(), other.getOwner() });
-	}
+    // Sobreescribimos hashCode para indicar la manera en que vamos a generar un
+    // hash
+    @Override
+    public int hashCode() {
+        return Objects.hash(getTokenAmount(), getOwner());
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(getTokenAmount(), getOwner());
-	}
+    // Sobreescribimos el metodo toString para definir como se va a devolver el
+    // valor cuando llamemos a este metodo.
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName() + "0" + Integer.toHexString(hashCode()) + " [tokenAmount=" + tokenAmount
+                + ", owner=" + owner + "]";
+    }
 
-	@Override
-	public String toString() {
-		return this.getClass().getSimpleName() + "@" + Integer.toHexString(hashCode()) + " [tokenAmount=" + tokenAmount
-				+ ", owner=" + owner + "]";
-	}
 }
